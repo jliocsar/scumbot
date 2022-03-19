67eda1627e2d7ac197a5c30f15c6bb2712869fd6
jliocsar <juliocesarmfo@gmail.com>
2022-03-19 09:52:14 -0300

    feat: bacon

--- app/index.ts
+++ app/index.ts
@@ -1,9 +1,46 @@
+import 'dotenv-safe/config'
 import type { Client } from 'discord.js'
 import signale from 'signale'
-import 'dotenv-safe/config'
+import axios from 'axios'
 
 import { createClient } from './client'
 import { setupClientEvents } from './events'
+
+/*********************
+ * Hacky keep-alive
+ * Get rid of this later!
+ ***********************/
+const MINUTE = 60 * 1000
+const BACON_IPSUM_API_URL = 'https://baconipsum.com/api/'
+const axiosClient = axios.create({ baseURL: BACON_IPSUM_API_URL })
+const customLogger = new signale.Signale({
+  types: {
+    bacon: {
+      badge: '🍖',
+      color: 'red',
+      label: 'bacon',
+      logLevel: 'info',
+    },
+  },
+})
+function keepBotAlive() {
+  setInterval(async () => {
+    const { data } = await axiosClient.get('/', {
+      params: {
+        type: 'meat-and-filler',
+        paras: 1,
+      },
+    })
+    const [quote] = data
+
+    customLogger.bacon('Keeping the bot alive with bacon:', quote)
+  }, MINUTE / 2)
+}
+keepBotAlive()
+/*********************
+ * Hacky keep-alive
+ * Get rid of this later!
+ ***********************/
 
 function setupClient(client: Client) {
   client.on('ready', () => {
